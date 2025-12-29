2d867b6ae7943b41535b0b1aca7e184e1378dd10
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-12-29 11:05:15 -0300

    feat: add menu and hours routes to web server

--- cap03/restaurant_web_server/index.js
+++ cap03/restaurant_web_server/index.js
@@ -7,5 +7,13 @@ app.get("/", async (request, reply) => {
     return "Welcome to What's Fare is fair!"
 });
 
+app.get("/menu", async (request, reply) => {
+    return "TODO: Menu page";
+});
+
+app.get("/hours", async (request, reply) => {
+    return "TODO: Hours page";
+});
+
 await app.listen({ port });
-console.log("Web server is running at http://localhost:${port}");
+console.log(`Web server is running at http://localhost:${port}`);
