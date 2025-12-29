597e80385f1fb04412d02f4d1529b883b02c9c70
Luiz Eduardo <luiz.earaujo@gmail.com>
2025-12-29 14:30:37 -0300

    feat: add minimatch and picocolors packages with necessary files
    - Added minimatch package with version 5.1.6 including its package.json.
    - Introduced picocolors package with version 1.1.1, including its README, LICENSE, and necessary JavaScript and TypeScript files.
    - Created a simple index.ejs view for the restaurant web server.

--- cap03/restaurant_web_server/index.js
+++ cap03/restaurant_web_server/index.js
@@ -1,12 +1,20 @@
+import ejs from "ejs";
 import Fastify from "fastify";
+import fastifyView from "@fastify/view";
 import operationHours from "./data/operationHours.js";
 import menu from "./data/menuItem.js";
 
 const app = Fastify();
 const port = 3000;
 
-app.get("/", async (request, reply) => {
-    return "Welcome to What's Fare is fair!"
+app.register(fastifyView, {
+    engine: {
+        ejs: ejs,
+    },
+});
+
+app.get("/", (request, reply) => {
+       reply.view("views/index.ejs", { name: "What's Fare is Fair" });
 });
 
 app.get("/menu", async (request, reply) => {
@@ -17,5 +25,7 @@ app.get("/hours", async (request, reply) => {
     return operationHours;
 });
 
-await app.listen({ port });
-console.log(`Web server is running at http://localhost:${port}`);
+await app.listen({ port }, (err, address) => {
+    if (err) throw err;
+    console.log(`Server listening at ${address}`);
+});
